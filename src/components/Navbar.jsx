628c87bbec8ba01f7f6d13fea00462df032ee904
nima <nimasaghi.dev@gmail.com>
2025-03-21 14:48:00 +0100

    feat:add  mobile-drawer icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
+import { Menu, X } from 'lucide-react'; 
+import { useState } from 'react'; // to make the icons dynamic
 import logo from '../assets/logo.png';
 import { navItems } from '../constants';
 
 const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+    const toggleNavBar = () => {
+        setMobileDrawerOpen(!mobileDrawerOpen);
+    }
+
     return (
         <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
             <div className="container px-4 mx-auto relative text-sm">
-                <div className="flex justify-center items-center">
+                <div className="flex justify-between items-center">
                     <div className="flex items-center flex-shrink-0">
                         <img className='h-10 w-10 mr-2' src={logo} alt="logo" />
                         <span className="text-xl tracking-tight">Landing Page</span>
@@ -24,6 +32,11 @@ const Navbar = () => {
                         <a href="#" className='bg-gradient-to-r from-orange-500 to orange-800 py-2 px-3 rounded-md'>
                             Create an account
                         </a>
+                    </div>
+                    <div className="lg:hidden md:flex flex-col justify-end">
+                        <button onClick={toggleNavBar}>
+                            {mobileDrawerOpen ? <X /> : <Menu />}
+                        </button>
                     </div>
                 </div>
             </div>
