ec96751e15e9c80f4b3e1d8997dd8d0fac7cba55
nima <nimasaghi.dev@gmail.com>
2025-03-21 15:27:14 +0100

    feat:add text to the hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,15 @@
-
+import video1 from '../assets/video1.mp4';
+import video2 from '../assets/video2.mp4';
 const HeroSection = () => {
     return (
-        <div>HeroSection</div>
+        <div className="flex flex-col items-center mt-6 lg:mt-20">
+            <h1 className="text4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+                Virtual build events 
+                <span className='bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text'>
+                {" "}for developers
+                </span>
+            </h1>
+        </div>
     )
 }
 
