d6a504a9e776fe7439961532ca74120543e4c888
nima <nimasaghi.dev@gmail.com>
2025-03-21 12:50:33 +0100

    feat:add all items to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/logo.png';
+import { navItems } from '../constants';
 
 const Navbar = () => {
     return (
@@ -8,6 +9,21 @@ const Navbar = () => {
                     <div className="flex items-center flex-shrink-0">
                         <img className='h-10 w-10 mr-2' src={logo} alt="logo" />
                         <span className="text-xl tracking-tight">Landing Page</span>
+                    </div>
+                    <ul className='hidden lg:flex ml-14 space-x-12'>
+                        {navItems.map((item, index) => (
+                            <li key={{ index }}>
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="hidden lg:flex justify-center space-x-12 items-center">
+                        <a href="#" className='py-2 px-3 border rounded-md'>
+                            Sign In
+                        </a>
+                        <a href="#" className='bg-gradient-to-r from-orange-500 to orange-800 py-2 px-3 rounded-md'>
+                            Create an account
+                        </a>
                     </div>
                 </div>
             </div>
