608020eb09a36a88a49046c809ed055071896821
nima <nimasaghi.dev@gmail.com>
2025-03-21 15:33:46 +0100

    feat:add paragraph and button under it

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,6 +9,12 @@ const HeroSection = () => {
                 {" "}for developers
                 </span>
             </h1>
+            <p className="text-center text-lg text-neutral-500 mt-10 max-w-4xl">
+                Empower your creativity and bring your VR app ideas to life with our intuitive development platform. Get started today and turn your vision into immersive reality.
+            </p>
+            <div className="flex justify-center my-10">
+                <a href="#" className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 rounded-md '>Start for FREE</a>
+            </div>
         </div>
     )
 }
