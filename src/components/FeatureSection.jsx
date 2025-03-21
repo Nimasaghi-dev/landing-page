2a0009a61c10ecbedef2814f342818f25a744b10
nima <nimasaghi.dev@gmail.com>
2025-03-21 20:01:10 +0100

    feat:feature section text implemented

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -7,6 +7,12 @@ const FeatureSection = () => {
                 <span className='bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase'>
                     Features
                 </span>
+                <h2 className='text-3xl sm:text-5xl lg:text-6xl mt-6 tracking-wide'>
+                    Build with 
+                    <span className='bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text'>
+                        {" "}confidence
+                    </span>
+                </h2>
             </div>
         </div>
     )
